4115e8ed99bc20a28d961724bf1a2f3bcbd5fcd6
Sean Wilkinson <sean@mathbiol.org>
2014-11-11 17:55:50 -0600

    Added some dependencies and a placeholder source file

---
+++ R/main.r
@@ -0,0 +1,19 @@
+#-  R source code
+
+#-  main.r ~~
+#
+#   This file is mostly a placeholder at the moment ...
+#
+#                                                       ~~ (c) SRW, 11 Nov 2014
+#                                                   ~~ last updated 11 Nov 2014
+
+library(jsonlite)
+library(RCurl)
+
+main <- function() {
+  # This function is just a placeholder right now, rather than the "main" entry
+  # point for execution that the name would suggest ...
+    cat('Hooray, it worked!\n')
+}
+
+#-  vim:set syntax=r:
