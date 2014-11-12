20bed30c2cc7fc931c5d870ad3b411a635723e53
Sean Wilkinson <sean@mathbiol.org>
2014-11-11 20:53:52 -0600

    Switched to "httr" package and implemented all 3 API calls in basic form

--- R/main.r
+++ R/main.r
@@ -7,8 +7,26 @@
 #                                                       ~~ (c) SRW, 11 Nov 2014
 #                                                   ~~ last updated 11 Nov 2014
 
-library(jsonlite)
-library(RCurl)
+library(httr)
+
+get_avar <- function(box = uuid(), key) {
+  # This function needs documentation.
+    if (missing(key)) {
+        stop('`get_avar` requires a known `key`.')
+    }
+    path = stringr::str_c('box/', box, '?key=', key, collapse = '')
+    req <- GET(mothership, path = path)
+    cat(content(req, as = 'text'), '\n', sep = '')
+    return(req)
+}
+
+get_jobs <- function(box = uuid(), status = 'waiting') {
+  # This function needs documentation.
+    path <- stringr::str_c('box/', box, '?status=', status, collapse = '')
+    req <- GET(mothership, path = path)
+    cat(content(req, as = 'text'), '\n', sep = '')
+    return(req)
+}
 
 main <- function() {
   # This function is just a placeholder right now, rather than the "main" entry
@@ -16,10 +34,30 @@ main <- function() {
     cat('Hooray, it worked!\n')
 }
 
+mothership <- 'https://api.qmachine.org/'
+
+set_avar <- function(box = mothership, key = uuid(), status = NULL, val = 0) {
+  # This function needs documentation.
+    avar <- list(box = box, key = key, val = jsonlite::serializeJSON(val))
+    if (missing(status) == FALSE) {
+        avar['status'] = status
+    }
+    req <- POST(mothership, content_type_json(),
+            path = stringr::str_c('box/', box, '?key=', key, collapse = ''),
+            body = jsonlite::toJSON(avar, auto_unbox = TRUE))
+    cat(content(req, as = 'text'), '\n', sep = '')
+    return(req)
+}
+
+submit <- function(x, f, box = uuid(), env = environment(f)) {
+  # This function needs documentation.
+    cat('(`submit` placeholder)\n')
+}
+
 uuid <- function() {
   # This function needs documentation.
     x <- sample(c(0:9, letters[1:6]), size = 32, replace = TRUE)
-    return(paste(x, collapse = ''))
+    return(stringr::str_c(x, collapse = ''))
 }
 
 #-  vim:set syntax=r:
